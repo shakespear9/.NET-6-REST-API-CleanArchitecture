1c02e46a8d0645ce1581d56d9ceebe4704b2d243
Nuttakorn Tedthong <leendsru@hotmail.com>
2022-07-12 09:50:38 +0700

    4.2 Global Exception Handler by Controller Filter

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,21 +1,26 @@
+using BuberDinner.Api.Filter;
 using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Application.Services.Authentication;
 using BuberDinner.Infrastructure;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration)
-        .AddControllers();
+        .AddControllers(options =>
+        {
+            options.Filters.Add<ErrorHandlingFilterAttribute>();
+        });
 
 }
 
 
 var app = builder.Build();
 {
-    app.UseMiddleware<ErrorHandlingMiddleware>();
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
